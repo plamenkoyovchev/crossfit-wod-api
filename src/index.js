050239f0b491812bca15a43e37a68f17606ac4d5
plamenkoyovchev <plamenkooo@gmail.com>
2023-01-19 20:10:47 +0200

    Add versioning.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
+const apiV1Router = require("./v1/routes");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("<h1>It's working!</h1>");
-});
+app.use("/api/v1", apiV1Router);
 
 app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`);
