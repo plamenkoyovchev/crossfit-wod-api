9991e434e42c2adc134d338a95a892b0b02bffca
plamenkoyovchev <plamenkooo@gmail.com>
2023-01-19 20:45:52 +0200

    Add body-parser in order to accept json.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
 const workoutV1Router = require("./v1/routes/workoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use("/api/v1/workouts", workoutV1Router);
 
 app.listen(PORT, () => {
