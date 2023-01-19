37553f39ca5479785444287570a22861f35e08b9
plamenkoyovchev <plamenkooo@gmail.com>
2023-01-19 20:36:00 +0200

    Add the service layer functions.

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,20 +1,33 @@
+const {
+	getAllWorkouts,
+	getWorkout,
+	createWorkout,
+	updateWorkout,
+	deleteWorkout,
+} = require("../services/workoutService");
+
 const getAllWorkouts = (req, res) => {
+	const allWorkouts = getAllWorkouts();
 	res.send("Get all workouts");
 };
 
 const getWorkout = (req, res) => {
+	const workout = getWorkout();
 	res.send("Get an existing workout");
 };
 
 const createWorkout = (req, res) => {
+	const createdWorkout = createWorkout();
 	res.send("Create a new workout");
 };
 
 const updateWorkout = (req, res) => {
+	const updatedWorkout = updateWorkout();
 	res.send("Update an existing workout");
 };
 
 const deleteWorkout = (req, res) => {
+	const deletedWorkout = deleteWorkout();
 	res.send("Delete an existing workout");
 };
 
---
+++ src/services/workoutService.js
@@ -0,0 +1,27 @@
+const getAllWorkouts = () => {
+	return;
+};
+
+const getWorkout = () => {
+	return;
+};
+
+const createWorkout = () => {
+	return;
+};
+
+const updateWorkout = () => {
+	return;
+};
+
+const deleteWorkout = () => {
+	return;
+};
+
+module.exports = {
+	getAllWorkouts,
+	getWorkout,
+	createWorkout,
+	updateWorkout,
+	deleteWorkout,
+};
